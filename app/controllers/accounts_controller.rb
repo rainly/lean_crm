3135f27b565cbd73fcbb4dd3928a964dc9252118
Matt Beedle <mattbeedle@googlemail.com>
2010-03-03 19:45:41 +0100

    Increased per page to 50 on contacts and accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -13,7 +13,7 @@ class AccountsController < InheritedResources::Base
 protected
   def collection
     @accounts ||= Account.permitted_for(current_user).not_deleted.order('name', 'asc').
-      paginate(:per_page => 10, :page => params[:page] || 1)
+      paginate(:per_page => 50, :page => params[:page] || 1)
   end
 
   def resource
