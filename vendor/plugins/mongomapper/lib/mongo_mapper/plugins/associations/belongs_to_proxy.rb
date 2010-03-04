0946550db4b4f175f3560f89a37114ea1888b778
Matt Beedle <mattbeedle@googlemail.com>
2010-03-04 18:45:30 +0100

    quick mongomapper fix

--- vendor/plugins/mongomapper/lib/mongo_mapper/plugins/associations/belongs_to_proxy.rb
+++ vendor/plugins/mongomapper/lib/mongo_mapper/plugins/associations/belongs_to_proxy.rb
@@ -17,7 +17,7 @@ module MongoMapper
         protected
           def find_target
             return nil if owner[association.foreign_key].nil?
-            klass.find_by_id(owner[association.foreign_key])
+            klass.to_s.constantize.find_by_id(owner[association.foreign_key])
           end
       end
     end
