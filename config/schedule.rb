082bd1a1d2d5cbd5ab096f23e13a5a76ef54e049
Matt Beedle <mattbeedle@googlemail.com>
2010-03-05 11:59:42 +0100

    Actually, schedule should be working now, paths are now absolute, not relative, which I don't think cron would have liked

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :environment, 'production'
 
 every 1.day, :at => '7am' do
-  command "./script/runner 'Task.daily_email'"
+  command "/var/www/salesflip/current/script/runner Task.daily_email"
 end
