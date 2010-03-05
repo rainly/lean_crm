ca7978c8217d5f738d58f4cae9ed8506be267a1f
Matt Beedle <mattbeedle@googlemail.com>
2010-03-05 11:27:01 +0100

    Added caching to javascripts

--- lib/useful_scopes.rb
+++ lib/useful_scopes.rb
@@ -1,8 +1,10 @@
 module UsefulScopes
   def self.included( base )
     base.class_eval do
-      named_scope :order, lambda { |order, direction| { :order => "#{order} #{direction}" } }
-      named_scope :limit, lambda { |limit| { :limit => limit } }
+      if base.respond_to?(:named_scope)
+        named_scope :order, lambda { |order, direction| { :order => "#{order} #{direction}" } }
+        named_scope :limit, lambda { |limit| { :limit => limit } }
+      end
     end
   end
 end
