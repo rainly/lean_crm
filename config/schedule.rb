7349192169c944ee6b3f84af566f4b6610a10fad
Matt Beedle <mattbeedle@googlemail.com>
2010-04-05 15:34:41 +0200

    hourly backup task?:

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,3 +8,7 @@ end
 every 10.minutes do
   rake 'sphinx:index'
 end
+
+every 1.hour do
+  command '/opt/mongo/bin/mongodump -d salesflip_production -o /root/backups/ && cd /root/backups/ && git add salesflip_production && git commit -m "Salesflip backup" && git push origin master'
+end
