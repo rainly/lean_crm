5b018fba7b3da2a3c7751bfac0b4e85f9a713baa
Matt Beedle <mattbeedle@googlemail.com>
2010-04-11 15:15:10 +0200

    Added right calendar js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,25 @@ var Base = new Class({
 
   initialize: function() {
     this.watchTitleTogglers();
+    this.addRealTaskCalendar();
   },
-  
+
+  addRealTaskCalendar: function() {
+    $('realdate').insert({
+      after: '<label for="realdate_input">Due At</label><input id="realdate_input" type="text" name="task_due_at"/>'
+    });
+    $('realdate').remove();
+    new Calendar({ format: "%Y-%m-%d %H:%M" }).assignTo('realdate_input');
+  },
+
   watchTitleTogglers: function() {
-    
+
     $$("div.toggle").each(function(div) { div.hide(); });
-    
+
     $$("h3.toggler").each(function(h3) {
-      
+
       h3.insert(new Element('span'), 'top');
-      
+
       if ( h3.hasClass('open') ) {
         h3.select('span')[0].update('&#9660;');
         h3.next('.toggle').show();
@@ -19,22 +28,19 @@ var Base = new Class({
       else {
         h3.select('span')[0].update('&#9654;');
       };
-      
+
       h3.onClick(function() {
         if ( this.hasClass('open') ) {
           this.select('span')[0].update('&#9654;');
         }
         else {
-           
            this.select('span')[0].update('&#9660;');
         };
-        
         this.next('.toggle').toggle('slide',{ duration:100 });
         this.toggleClass('open');
       });
     });
   }
-  
 });
 
 document.onReady(function() {
