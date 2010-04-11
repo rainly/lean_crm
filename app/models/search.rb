0c1a9bdadaec5280cd861966be4e0908dce9ca3f
Matt Beedle <mattbeedle@googlemail.com>
2010-04-11 19:16:38 +0200

    Added search by company

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,17 +1,35 @@
 class Search
   include MongoMapper::Document
 
-  key :terms,       String,   :required => true
+  key :terms,       String
   key :user_id,     ObjectId
   key :collections, Array
+  key :company,     String
   timestamps!
 
   belongs_to :user
 
+  validate :criteria_entered?
+
   def results
-    @results ||= (collections.blank? ? %w(Lead, Contact, Account) : collections).map do |klass|
-      klass = klass.constantize
-      klass.scoped(:conditions => { :id => klass.search(terms).map(&:id) }).permitted_for(user).not_deleted
-    end.inject { |sum,n| sum += n }
+    if company
+      @results ||= Lead.scoped(:conditions => { :id => Lead.search("@company #{company}").map(&:id) }).
+        permitted_for(user).not_deleted +
+        Account.scoped(:conditions => { :id => Account.search("@name #{company}").map(&:id) }).
+        permitted_for(user).not_deleted
+    else
+      @results ||= (collections.blank? ? %w(Lead, Contact, Account) : collections).map do |klass|
+        klass = klass.constantize
+        klass.scoped(:conditions => { :id => klass.search(terms).map(&:id) }).permitted_for(user).not_deleted
+      end.inject { |sum,n| sum += n }
+    end
+  end
+
+private
+  def criteria_entered?
+    if self.terms.blank? and self.company.blank?
+      self.errors.add :terms, I18n.t('activerecord.errors.messages.blank')
+      self.errors.add :company, I18n.t('activerecord.errors.messages.blank')
+    end
   end
 end
--- test/unit/search_test.rb
+++ test/unit/search_test.rb
@@ -2,14 +2,31 @@ require 'test_helper.rb'
 
 class SearchTest < ActiveSupport::TestCase
   context 'Class' do
-    should_have_key :terms, :user_id, :created_at, :updated_at, :collections
-    should_require_key :terms
+    should_have_key :terms, :user_id, :created_at, :updated_at, :collections, :company
     should_belong_to :user
   end
 
   context 'Instance' do
     setup do
       @search = Search.make_unsaved
+    end
+
+    should 'require terms if no company text' do
+      @search.terms = nil
+      @search.company = nil
+      assert !@search.valid?
+      assert @search.errors.on(:terms)
+      @search.company = 'asfefa'
+      assert @search.valid?
+    end
+
+    should 'require company if no terms set' do
+      @search.terms = nil
+      @search.company = nil
+      assert !@search.valid?
+      assert @search.errors.on(:company)
+      @seach.terms = 'asfef'
+      assert @search.valid?
     end
   end
 end
