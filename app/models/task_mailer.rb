8b4d63b5de3b48e0957313f16d21fc4ae0ccadbc
Matt Beedle <mattbeedle@googlemail.com>
2010-04-12 10:13:43 +0200

    Task mailer fix

--- app/models/task_mailer.rb
+++ app/models/task_mailer.rb
@@ -2,7 +2,7 @@ class TaskMailer < ActionMailer::Base
 
   def assignment_notification( task )
     recipients    task.assignee.email
-    from          I18n.t('emails.do_not_reply')
+    from          I18n.t('emails.do_not_reply', :host => 'salesflip.com')
     subject       I18n.t('emails.task_reassigned.subject')
     body          :url => task_url(task)
     sent_on       Time.now
@@ -10,7 +10,7 @@ class TaskMailer < ActionMailer::Base
 
   def daily_task_summary( user, tasks )
     recipients    user.email
-    from          I18n.t('emails.do_not_reply')
+    from          I18n.t('emails.do_not_reply', :host => 'salesflip.com')
     subject       I18n.t('emails.daily_task_summary.subject', :date => Date.today.to_s(:long))
     body          :tasks => tasks
     sent_on       Time.now
