afc4a16c9856ef1cc916fca8c6c418cc42086a02
Matt <matt@matt-laptop.(none)>
2010-04-25 16:56:08 +0200

    Software marketplatz parser

--- config/initializers/string_extensions.rb
+++ config/initializers/string_extensions.rb
@@ -2,4 +2,8 @@ class String
   def to_dom_id
     self.downcase.gsub(/\s/, '-').gsub(/[^\w^\-]/, '')
   end
+
+  def strip_html
+    self.gsub(/<\/?[^>]*>/, "") if self
+  end
 end
---
+++ lib/software_marketplatz_parser.rb
@@ -0,0 +1,65 @@
+require 'open-uri'
+require 'hpricot'
+
+class SoftwareMarketplatzParser
+  def initialize
+    @parsed_pages = []
+    parse_index("http://#{uri.host}#{uri.path}")
+    doc.search('//a').each do |a|
+      if a.attributes['href'].match(/^f_liste.php'/)
+        parse_index("http://#{uri.host}/#{a.attributes['href']}")
+      end
+    end
+  end
+
+  def parse_index( page )
+    puts "parsing index: #{page}"
+    doc.search('//a').each do |a|
+      if a.attributes['href'].match(/^firmeninformation/)
+        parse_page(a.attributes['href']) unless @parsed_pages.include?(a.attributes['href'])
+      end
+    end
+  end
+
+  def parse_page( page )
+    puts "parsing page: #{page}"
+    lead = User.find_by_email('mattbeedle@googlemail.com').leads.build :rating => 3
+    doc = Hpricot(open("http://#{uri.host}/#{page}"))
+    fieldset = doc.search('//fieldset').first
+    fieldset.inner_html.split('<br />').each_with_index do |data, index|
+      case index
+      when 0 then lead.company = data.split('<b>').last.strip_html
+      when 1 then lead.address = data
+      when 2
+        data =~ /([0-9]{5})/
+        lead.postal_code = $1
+        lead.city = data.split(/\s/).last
+      when 3 then lead.phone = data
+      #when 4 then lead.fax = data
+      when 7
+        name = data.gsub(/,[^,]*/, '')
+        if result = name.match(/^[\w]{0,5}\./)
+          lead.title = result[0].gsub(/\./, '')
+          name.gsub!(/#{result[0]}/, '').strip
+        end
+        names = name.strip.split(/\s/)
+        lead.first_name = names.shift
+        lead.last_name = names.join(' ')
+      end
+    end
+    lead.save
+    @parsed_pages << page
+  end
+
+  def parsed_pages
+    @parsed_pages
+  end
+
+  def uri
+    @uri ||= URI.parse('http://www.software-marktplatz.de/itanbieter_deutschland-d.html')
+  end
+
+  def doc
+    @doc ||= Hpricot(open("http://#{uri.host}#{uri.path}"))
+  end
+end
