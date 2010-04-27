3b82fbdbbe594691fba9b968ef3ccf87af653c90
Matt <matt.beedle@1000jobboersen.de>
2010-04-27 20:26:25 +0200

    Commented out mail processor restart cron job, it wasn't working anyway

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,10 +5,10 @@ every 1.day, :at => '7am' do
   command "/var/www/salesflip/current/script/runner User.send_tracked_items_mail"
 end
 
-every 10.minutes do
-  rake 'sphinx:index'
-  command 'cd /var/www/salesflip/current && ruby lib/mail_processor_control.rb restart'
-end
+#every 10.minutes do
+#  rake 'sphinx:index'
+#  command 'cd /var/www/salesflip/current && ruby lib/mail_processor_control.rb restart'
+#end
 
 #every 1.hour do
 #  command '/opt/mongo/bin/mongodump -d salesflip_production -o /root/backups/ && cd /root/backups/ && git add salesflip_production && git commit -m "Salesflip backup" && git push origin master'
